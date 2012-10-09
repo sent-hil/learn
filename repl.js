73c29377aea53faa3a4cf37c828491396a707785
sent-hil <me@sent-hil.com>
2012-10-09 14:45:45 -0400

    modified repl

--- repl.js
+++ repl.js
@@ -1,5 +1,6 @@
 var stdout = process.stdout;
 var readline = require('readline');
+
 var repl = readline.createInterface({
   input: process.stdin,
   output: stdout
@@ -14,4 +15,35 @@ process.on("uncaughtException", function (error) {
 
 repl.on('line', function(cmd) {
   console.log(eval(cmd));
+});
+
+var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 27017;
+var connection = new net.Socket();
+var BSON = require('bson');
+var Buffer = require('buffer').Buffer;
+
+connection.connect(PORT, HOST, function() {
+  console.log('connected');
+
+  client = net.createConnection(PORT, HOST)
+  console.log(client);
+
+  db   = "sample_db"
+  coll = "test"
+  docs = [{a:1}]
+  msg  = ""
+  size = Buffer.byteLength(coll);
+
+  for(var i = 0; i < docs.length; i++) {
+    console.log('----')
+    console.log(BSON.calculateObjectSize(docs[i]));
+    console.log('----')
+  };
+
+  console.log(size);
+
+  //result = client.write(msg);
+  //console.log(result);
 });
